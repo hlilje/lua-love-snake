2076603d9d8c0a2ec7e870c602e4fc9b2cc74b2c
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-07-05 14:27:32 +0200

    Add Snakes

--- players.lua
+++ players.lua
@@ -2,8 +2,29 @@ require "constants"
 require "map"
 
 
+-- Add the given position to the front of the given snake.
+function addSnakePos(snake, x, y)
+    -- TODO: Check that there is no name clash
+    Snake1 = {next = Snake1, x = x, y = y}
+end
+
+-- Remove the tail of the given snake
+function delSnakePos(snake)
+    if snake == nil then return end
+
+    local prev = snake
+
+    while snake.next do
+        prev = snake
+        snake = snake.next
+    end
+    prev.next = nil
+end
+
 -- Create initial player positions.
 function createPlayers()
+    Snake1, Snake2 = nil, nil
+
     local xPosP1, yPosP1, xPosP2, xPosP2 = 1, 1, 1, 1
 
     -- Randomise starting positions for players until they are unblocked
@@ -16,6 +37,8 @@ function createPlayers()
         xPosP2 = love.math.random(1, MAP_WIDTH)
         yPosP2 = love.math.random(1, MAP_HEIGHT)
     until not isBlocked(xPosP2, yPosP2)
+
+    Snake1 = addSnakePos(Snake1, xPosP1, yPosP1)
 
     TileMap[yPosP1][xPosP1] = TILE_PLAYER_1
     TileMap[yPosP2][xPosP2] = TILE_PLAYER_2
