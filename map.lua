9547e35f0259a1cbecfad50eb631a7f186bcd7a8
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-08-02 14:31:46 +0200

    Generate random map borders

--- map.lua
+++ map.lua
@@ -3,6 +3,50 @@ require "globals"
 require "localisation"
 
 
+-- Generate solid map borders.
+function createSolidMapBorder()
+    for i = 1, NUM_TILES_X do TileMap[1][i]           = TILE_BLOCKED end
+    for i = 1, NUM_TILES_X do TileMap[NUM_TILES_Y][i] = TILE_BLOCKED end
+    for i = 1, NUM_TILES_Y do TileMap[i][1]           = TILE_BLOCKED end
+    for i = 1, NUM_TILES_Y do TileMap[i][NUM_TILES_X] = TILE_BLOCKED end
+end
+
+-- Generate one random map border.
+-- {x, y}Static ~= nil => use loop index. limit = iteration limit.
+function createRandomMapBorder(xStatic, yStatic, limit)
+    assert(not (xStatic == nil and yStatic == nil))
+
+    local remaining, length = limit, 0
+    -- Randomise whether to start blocking or not
+    local block = (love.math.random(0, 1) == 1) and false or true
+
+    -- Fill alternating blocked/free segments of random length
+    local i = 1
+    while i <= limit do
+        length = love.math.random(1, remaining)
+        if not block then goto continue end
+        for j = i, (i + length - 1) do
+            if xStatic then
+                TileMap[j][xStatic] = TILE_BLOCKED
+            elseif yStatic then
+                TileMap[yStatic][j] = TILE_BLOCKED
+            end
+        end
+        ::continue::
+        block = not block
+        remaining = remaining - length
+        i = i + length
+    end
+end
+
+-- Generate random map borders.
+function createRandomMapBorders()
+    createRandomMapBorder(nil, 1, NUM_TILES_X)
+    createRandomMapBorder(nil, NUM_TILES_Y, NUM_TILES_X)
+    createRandomMapBorder(1, nil, NUM_TILES_Y)
+    createRandomMapBorder(NUM_TILES_X, nil, NUM_TILES_Y)
+end
+
 -- Create the game map.
 function createMap()
     TileMap = {} -- [y][x]
@@ -28,10 +72,7 @@ function createMap()
     end
 
     -- Set all surrounding tiles as occupied
-    for i = 1, NUM_TILES_X do TileMap[1][i]           = TILE_BLOCKED end
-    for i = 1, NUM_TILES_X do TileMap[NUM_TILES_Y][i] = TILE_BLOCKED end
-    for i = 1, NUM_TILES_Y do TileMap[i][1]           = TILE_BLOCKED end
-    for i = 1, NUM_TILES_Y do TileMap[i][NUM_TILES_X] = TILE_BLOCKED end
+    createRandomMapBorders()
 end
 
 -- Randomly place one food on the map.
